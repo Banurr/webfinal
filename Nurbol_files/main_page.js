9f0670ef6f6b974240c305e2e060818141bbe39c
Nurbol Yermagambetov <nurbolyermagambetov@MacBook-Pro-Nurbol.local>
2022-12-19 03:10:16 +0600

    po idee zhatu kerek

--- Nurbol_files/main_page.js
+++ Nurbol_files/main_page.js
@@ -2,3 +2,12 @@ function go()
 {
     window.location.href = "main_page.html";
 }
+function go1()
+{
+    window.location.href = "http://localhost:63342/untitled/Web/webfinal/Azikosh%20files/tour.html?_ijt=rbja09a28e5c49a6io1c49lps5&_ij_reload=RELOAD_ON_SAVE";
+}
+function scrollToTop()
+{
+
+    $('html, body').animate({scrollTop:0},'50');
+}
