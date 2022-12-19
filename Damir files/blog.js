eaa134393aee97975d8102d9d94f8ee79274d902
Zhanibekov Damir <damirzhanibek04@gmail.com>
2022-12-20 01:27:02 +0600

    damir

--- Damir files/blog.js
+++ Damir files/blog.js
@@ -10,6 +10,30 @@ $(document).scroll(function()
     }
 });
 $("#blog_1_div1").click(function() {
-    $("#nop").show("200");
+    $("#blog_10").show("200");
     $("#blog_1").hide("200");
+    $("#head").hide("200");
+    $("#ending").hide("200");
+    $("#scrrr").hide("200");
+});
+$("#blog_1_div2").click(function() {
+    $("#blog_20").show("200");
+    $("#blog_1").hide("200");
+    $("#head").hide("200");
+    $("#ending").hide("200");
+    $("#scrrr").hide("200");
+});
+$("#blog_10_button").click(function() {
+    $("#blog_10").hide("200");
+    $("#blog_1").show("200");
+    $("#head").show("200");
+    $("#ending").show("200");
+    $("#scrrr").show("200");
+});
+$("#blog_20_button").click(function() {
+    $("#blog_20").hide("200");
+    $("#blog_1").show("200");
+    $("#head").show("200");
+    $("#ending").show("200");
+    $("#scrrr").show("200");
 });
