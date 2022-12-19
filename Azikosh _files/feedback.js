256698094723acf1a61c4582f94715abdbf2cf45
azikush <210107076@stu.sdu.edu.kz>
2022-12-19 17:00:19 +0600

    Neeeew

--- Azikosh _files/feedback.js
+++ Azikosh _files/feedback.js
@@ -1,3 +1,15 @@
 function go(){
     window.location.href = "feedback.html";
 }
+
+$(document).scroll(function()
+{
+    var tt = $(this).scrollTop();
+    if (tt > 4916 / 2)
+    {
+        $("#scr").fadeIn();
+    } else
+    {
+        $("#scr").fadeOut();
+    }
+});
