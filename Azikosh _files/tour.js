ba3a16355c78c2bb5b7d8c1634e339f7036032d1
azikush <210107076@stu.sdu.edu.kz>
2022-12-20 17:04:26 +0600

    new js

--- Azikosh _files/tour.js
+++ Azikosh _files/tour.js
@@ -9,3 +9,21 @@ $(document).scroll(function()
         $("#scrrr").fadeOut();
     }
 });
+
+$('#div_1').hover(function(){
+    $(this).find("#div_2").stop( true, true ).slideDown();
+}, function() {
+    $(this).find("#div_2").stop( true, true ).slideUp();
+})
+
+$('#div_3').hover(function(){
+    $(this).find("#div_4").stop( true, true ).slideDown();
+}, function() {
+    $(this).find("#div_4").stop( true, true ).slideUp();
+})
+
+$('#div_5').hover(function(){
+    $(this).find("#div_6").stop( true, true ).slideDown();
+}, function() {
+    $(this).find("#div_6").stop( true, true ).slideUp();
+})
