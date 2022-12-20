127f87312b95f8d02f880f56377fc77ba6e00c69
Nurbol Yermagambetov <nurbolyermagambetov@MacBook-Pro-Nurbol.local>
2022-12-20 13:05:31 +0600

    ffff

--- Nurbol_files/sign.php
+++ Nurbol_files/sign.php
@@ -2,7 +2,7 @@
     define('DB_SERVER' , 'localhost');
     define('DB_USERNAME' , 'root');
     define('DB_PASSWORD' , '');
-    define('DB_NAME' , 'tour');
+    define('DB_NAME' , 'final');
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     if($link === false)
     {
